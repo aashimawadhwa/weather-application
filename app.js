7d48b933309e36c96a52f8269d9f6d84b0cd96c7
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-07-10 02:32:39 +0530

    added weaher application

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const https = require("https")
+const app= express();
+
+app.get ("/" , function (req, res){
+    res.send("server is running.")
+    const url ="https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=cbeb3c125067510dcb0d8ecb1ae224a2"
+    https.get ("url,function(response){")
+})
+
+
+app.listen(3000, function(){
+    console.log("heyy nummu")
+})
