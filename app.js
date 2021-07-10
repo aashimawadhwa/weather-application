e60c7a7a0364d40de8fd2783bc9f250d06fb05d1
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-07-11 03:44:15 +0530

    added weaher application

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ app.get ("/" , function (req, res){
         const weatherData = JSON.parse(data)
         const  temp = weatherData.main.temp
         const weatherDescription = weatherData.weather[0].description
+        const icon = weatherData.weather[0].icon
+        const imageURL="http://openweathermap.org/img/wn/"+ icon+ "@2x.png"
         res.write("<h2>The Temprature in Delhi is "+temp +" degree celcuis</h2>")
         res.write("<h2>The Weather currently is "+ weatherDescription +"</h2> ")
-
+        res.write("<img src=" + imageURL + ">");
         res.send()
     }) 
      })
