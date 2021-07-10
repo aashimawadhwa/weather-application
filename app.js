4e30ae92cf40e566bd57ce5ddeed51ab8c26722d
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-07-11 03:02:37 +0530

    added weatherapplication

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const https = require("https")
 const app= express();
 
 app.get ("/" , function (req, res){
-    res.send("server is running.")
+   
     const url ="https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=cbeb3c125067510dcb0d8ecb1ae224a2"
-    https.get ("url,function(response){")
-})
+    
+    
+    https.get (url,function(response){
+        console.log(response.statuscode);
+       
+        response.on("data" , function(data){
+        const weatherData = JSON.parse(data)
+        const object={
+            name:"Jaadu",
+            favoriteFood:"baraf"
+        }
+      console.log(  JSON.stringify(object));
+        })
 
+    })
+    res.send("server is running.")
+})
 
 app.listen(3000, function(){
     console.log("heyy nummu")
